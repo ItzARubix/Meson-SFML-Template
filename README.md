e90c78a275e4e0f3ae4e7f4b18437fe57ed0ef50
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2025-01-19 18:48:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# NylCaster
+An SFML demonstration utilizing the Meson SFML template
+
+# Dependencies
+NylCaster relies on [Simple and Fast Multimedia Library](https://www.sfml-dev.org/). You may use a systemwide installation of SFML, but this project also provides SFML as a Git submodule in case you are unable (or do not want to) install SFML systemwide. Instructions on how to use a systemwide SFML install and how to use the Git subproject are provided below (using a systemwide SFML install will make the compile time of this project much faster).
+
+(LINUX ONLY) If you intend to use the Git submodule for SFML, you need to install all of SFML's dependencies, which allow SFML to be compiled. Here's what you need:
+- `libxrandr-dev`
+- `libxcursor-dev`
+- `libudev-dev`
+- `libflac-dev`
+- `libvorbis-dev`
+- `libgl1-mesa-dev`
+- `libegl1-mesa-dev`
+- `libdrm-dev`
+- `libgbm-dev`
+
+(I imagine that the non-dev versions of all of these packages work as well). Use your distro's package manager to install all of these. (On Windows, I'm pretty sure SFML uses packages that are shipped with the OS, so you don't need to install anything additional).
+
+Finally, make sure you have both Meson and CMake installed and available in your PATH. 
+
+# Compilation/Installation guide
+This project uses `meson`, a build tool that automatically handles compilation and linking.
+
+In order to build NylCaster and run it, you must ensure that you have `meson` (and all its dependencies, notably `ninja`) on your system. It is recommended that you do this through your distro's package manager, but it can also be done through `pip`. See [Meson's installation page](https://mesonbuild.com/Getting-meson.html) for more information.
+
+## If you have a systemwide SFML install
+- Make sure Meson and SFML are both installed
+- Clone and `cd` into this repository
+- Run `meson setup builddir -DuseSystemSFML=true`
+- Run `cd builddir`
+- Run `meson compile`
+- The executable should now be in the build directory. To run it, run `./NylCaster`
+
+## If you want to use the SFML submodule
+- Make sure Meson and CMake are both installed
+- Clone and `cd` into this repository
+- Run `meson setup builddir`
+- Run `cd builddir`
+- Run `meson compile`
+- The executable should now be in the build directory. To run it, run `./NylCaster`
+
+# Additional Notes
+The executable requires the user to create a `level.txt` file in the same directory as the executable in order for the exeuctable to run. This can be done with `echo "10.0 -0.5 10.0 0.5" > level.txt` in Bash while you're inside the `builddir`, for example.  
---
+++ main.cpp
@@ -0,0 +1,220 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+// Let it be known that I thusly have no idea how classes work in C++ lol. 
+
+class Player { // Player class might be moved to a new file at some point. This is just for organization's sake.
+	public:
+		sf::Vector2<float> position;
+		sf::Vector2<float> velocity;
+		sf::Vector2<float> vision;
+		Player() : position(0.f, 0.f), velocity(0.f, 0.f), vision(1.f, 0.f) {
+			
+		}
+		~Player() {
+			
+		}
+};
+
+class Wall {
+	public:
+		sf::Vector2<float> left;
+		sf::Vector2<float> right;
+		Wall(float x_1, float y_1, float x_2, float y_2) {
+			left.x = x_1;
+			left.y = y_1;
+			right.x = x_2;
+			right.y = y_2;
+		}
+		~Wall() {
+
+		}
+};
+
+
+void rayCastingStep(sf::RenderWindow& window, Player player, std::vector<Wall> wallList) {
+	constexpr float camToScreenDistance = 320/std::tan(2*M_PI/9);
+	for(int i=-320; i<=320; i++) {
+		sf::Angle rayDirectionAngle = sf::radians(std::atan(i/camToScreenDistance));
+		sf::Vector2 ray = 100.f*player.vision.rotatedBy(rayDirectionAngle);
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X)) {
+			std::cout << "For iteration " << i << " the rayDirectionAngle is " << rayDirectionAngle.asDegrees() << "\n";
+		}
+		
+		// IT'S TIME FOR THE WORST VARIABLE NAMES KNOWN TO MANKIND YAYYYY
+		// For the sake of not obfuscating what I'm doing here, https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line_segment
+		// Godspeed
+		sf::Vector2 currentShortestDisplacement(1000.f, 0.f);
+		for (Wall wall : wallList) {
+			float x_1 = player.position.x;
+			float y_1 = player.position.y;
+			float x_2 = x_1 + ray.x;
+			float y_2 = y_1 + ray.y;
+			float x_3 = wall.left.x;
+			float y_3 = wall.left.y;
+			float x_4 = wall.right.x;
+			float y_4 = wall.right.y;
+			float t_numerator = ((x_1-x_3)*(y_3-y_4)) - ((y_1-y_3)*(x_3-x_4));
+			float t_denominator = ((x_1-x_2)*(y_3-y_4)) - ((y_1-y_2)*(x_3-x_4));
+			float s_numerator = -1*(((x_1-x_2)*(y_1-y_3)) - ((y_1-y_2)*(x_1-x_3)));
+			float s_denominator = ((x_1-x_2)*(y_3-y_4)) - ((y_1-y_2)*(x_3-x_4));
+			if( (std::abs(t_numerator) < std::abs(t_denominator)) && (t_numerator*t_denominator > 0) && (std::abs(s_numerator) < std::abs(s_denominator)) && (s_numerator*s_denominator > 0) ) {
+				float t = t_numerator/t_denominator;
+				sf::Vector2 intersectionPoint(x_1 + t*(x_2-x_1), y_1 + t*(y_2-y_1));
+				sf::Vector2 displacementVector = intersectionPoint-player.position;
+				if( displacementVector.lengthSquared() < currentShortestDisplacement.lengthSquared() ) {
+					currentShortestDisplacement = displacementVector;
+				}
+			}
+		}
+		float distance = currentShortestDisplacement.length();
+		float apparentHeight = 360/(std::abs(std::cos(rayDirectionAngle.asRadians()))*distance);
+		float screenColumn = i+320.f;
+		sf::VertexArray line(sf::PrimitiveType::Lines, 2);  
+		line[0] = sf::Vertex{sf::Vector2f(screenColumn, 180-(apparentHeight/2))};
+		line[1] = sf::Vertex{sf::Vector2f(screenColumn, 180+(apparentHeight/2))};
+		sf::Color lineColor;
+		if(distance<1) {
+			lineColor.r = 255;
+			lineColor.g = 255;
+			lineColor.b = 255;
+			// lineColor = (255,255,255);
+		} else {
+			// lineColor = (255/distance, 255/distance, 255/distance);
+			lineColor.r = 255/distance;
+			lineColor.g = 255/distance;
+			lineColor.b = 255/distance;
+		}
+		line[0].color = lineColor;
+		line[1].color = lineColor;
+		window.draw(line);
+	}
+}
+
+
+int main() {
+
+	// It is helpful to imagine the window as existing in worldspace. I precalculated this on paper, but this number is dependent on the FOV, so I might have to change it later.
+	constexpr float camToScreenDistance = 320/std::tan(2*M_PI/9);
+	
+	// Create a player.
+	Player player;
+
+	// Create the level. You can edit this if you want to create a different layout of walls.
+	std::ifstream levelFile("level.txt");
+	if(!levelFile) {
+		std::cerr << "Failed to open level file. Make sure that you have a file named \"level.txt\" in the directory that the binary is stored, that includes the coordinates of a wall on each line (each line should have four numbers, two floats that represent the left edge of the wall in worldspace, and two floats that represent the right edge).\n";
+		return 1;
+	}
+	std::cout << "Make sure your level file has no commas or anything other than floats. One line might look like\n10.0 -5.0 10.0 5.0\nIf your level file has unexpected characters this could lead to unexpected behavior which I can't be bothered to check for.\n";
+	std::string line;
+	std::vector<Wall> level;
+	float leftEdgeX, leftEdgeY, rightEdgeX, rightEdgeY;
+	while (std::getline(levelFile, line)) {
+		std::istringstream lineStream(line);
+
+		lineStream >> leftEdgeX >> leftEdgeY >> rightEdgeX >> rightEdgeY;
+		level.emplace_back(leftEdgeX, leftEdgeY, rightEdgeX, rightEdgeY);
+		
+	}
+	
+
+	// Create the clock and a time variable to store deltaTime.
+	sf::Clock clock;
+	sf::Time deltaTime;
+
+	// Create the window.
+	sf::RenderWindow window(sf::VideoMode({640,360}), "NylCaster");
+
+	while (window.isOpen()) { // Primary execution loop. This will run for as long as the window is open. 
+
+		while (const std::optional event = window.pollEvent()) { // This while-loop condition will only fail if there are no events left in the window.pollEvent() queue. Thus, for each iteration of this loop, until it fails, it pops one event off the queue, and holds onto it in the event variable...
+
+			if (event->is<sf::Event::Closed>()) { // ... and if that event is sf::Event::Closed...
+				window.close(); // ... close the window.
+			}
+
+		} // If the program makes it out of this loop, that means we can start the actual logic of the frame.
+
+		// CAMERA LOGIC.
+		// 1. Define deltaCamAngle, the amount that CamAngle should change each frame.
+		sf::Angle deltaCamAngle = sf::degrees(35*deltaTime.asSeconds());
+		// 2. If the player presses Left, the vision vector should be rotated negatively by deltaCamAngle.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+			player.vision = player.vision.rotatedBy(-1.f*deltaCamAngle);
+		}
+		// 3. If the player presses Right, the vision vector should be rotated positively by deltaCamAngle.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+			player.vision = player.vision.rotatedBy(deltaCamAngle);
+		}
+
+		// VELOCITY LOGIC.
+		// 1. Define playerSpeed, a scalar that we'll use to ensure the magnitude of the player velocity remains consistent.
+		float playerSpeed = 2.f;
+		// 2. Create a temporary vector that encodes all the directional information about the player movement.
+		sf::Vector2 velocityDirection(0.f, 0.f);
+		// 3. Forward/backward direction is governed by player.vision.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+			velocityDirection += player.vision;
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+			velocityDirection -= player.vision;
+		}
+		// 4. Strafing is governed by a vector orthogonal to player.vision, which we define and then use.
+		sf::Vector2 strafeVector = player.vision.rotatedBy(sf::degrees(90));
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+			velocityDirection += strafeVector;
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			velocityDirection -= strafeVector;
+		}
+		// 5. We want player.velocity to be in the direction of velocityDirection and with the magnitude of playerSpeed.
+		if(velocityDirection.lengthSquared()!=0) {
+			player.velocity = playerSpeed*velocityDirection.normalized();
+		} else {
+			player.velocity = velocityDirection;
+		}
+
+		// POSITION LOGIC.
+		// 1. Add the player's velocity to their position, scaled by deltaTime.
+		player.position += deltaTime.asSeconds()*player.velocity;
+		// It should be noted that this is essentially just deltaX = velocity*deltaTime, 
+		// which you should know from physics class.
+
+		// (DEBUG) print all information about player if they press Z.
+		// This is done at the end, right before the draw step. 
+		// This is because we want the debug info to reflect changes made in the current frame.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z)) {
+			std::cout << "DEBUG: \n";
+			std::cout << "Player position: \n";
+			std::cout << "X: " << player.position.x << " Y: " << player.position.y << "\n";
+			std::cout << "Player velocity: \n";
+			std::cout << "X: " << player.velocity.x << " Y: " << player.velocity.y << "\n";
+			std::cout << "Player vision: \n";
+			std::cout << "X: " << player.vision.x << " Y: " << player.vision.y << "\n";
+			std::cout << "Player angle (relative to positive X): " << player.vision.angle().asDegrees() << "\n";
+			std::cout << "deltaTime: " << deltaTime.asSeconds() << "\n";
+			std::cout << "camToScreenDistance: " << camToScreenDistance << "\n";
+		}
+
+		// This is the "draw step". It's where we actually draw everything to the screen.
+		// 1. Clear the framebuffer.
+		window.clear();
+
+		// 2. Draw everything to the framebuffer that you must.
+		// RAYCASTING LOGIC.
+		rayCastingStep(window, player, level);
+
+		// 3. Draw the framebuffer.
+		window.display();
+
+		// 4. Save the deltaTime and restart the clock.
+		deltaTime = clock.restart();
+	}
+
+	return 0;
+}
